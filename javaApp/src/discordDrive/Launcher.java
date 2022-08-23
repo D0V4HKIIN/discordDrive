50fa2928434f1693a403555504cabde30cc76fe6
jonas <>
2022-08-23 12:33:44 +0200

    changed default folder. Should be handled better...

--- javaApp/src/discordDrive/Launcher.java
+++ javaApp/src/discordDrive/Launcher.java
@@ -1,7 +1,7 @@
 package discordDrive;
 
 public class Launcher {
-	static private String path = "/home/jonas/Sync/projects/code/discordDrive";
+	static private String path = "/home/jonas/Documents/code/discordDrive";
 
 	public static void main(String[] args) {
 		Viewer view = new Viewer(path);
